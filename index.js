b32c989ed19a9c026ecf135f5679b448318dd9a4
javierAguerregaray <gravedadsur@gmail.com>
2023-04-19 11:33:36 -0300

    actualizacion

--- index.js
+++ index.js
@@ -4,24 +4,35 @@ const listado = document.getElementById ("listado");
 
 
 
-function simpson() {fetch("https://apisimpsons.fly.dev/api/personajes?limit=900&page=1")
+function simpson() {fetch("https://apisimpsons.fly.dev/api/personajes?limit=720&page=1")
 .then(res => res.json())
 .then (datoslistos => pantalla(datoslistos.docs))
 
 }
 
+const parapantalla = document.getElementById ("tarjetas")
 function pantalla(arr) {
    for (let i = 0; i < arr.length; i++) {
       const tarjeta = document.createElement("div");
-      tarjeta.innerHTML =
-      arr[i].nombre
-   
-   
+    tarjeta.innerHTML = 
+    
+    
+    `
+    <img src="${arr[i].Imagen}" " width="100">
+    "${arr[i].Nombre}"
+    
+    `
+
+  parapantalla.appendChild(tarjeta)      
    }
 }
 simpson()
 pantalla()
-/*.then (datoslistos => console.log (datoslistos.results))
+/*
+
+console.log(arr)
+   
+.then (datoslistos => console.log (datoslistos.results))
 const tarhtml = document.getElementById ("tarjetas")
 
 function pantalla(arr) {
